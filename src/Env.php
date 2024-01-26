ac2f11c90dfb487af4e694ba34447bca65fba67a
Dmitry <40324321+inilim@users.noreply.github.com>
2024-01-26 14:48:42 +0300

    ref

---
+++ src/Env.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Inilim\TaskManager;
+
+use Inilim\Array\Array_;
+use Inilim\Integer\Integer;
+use Inilim\JSON\JSON;
+use Inilim\TaskManager\IPDO;
+
+class Env
+{
+    public static array $components = [
+        'json' => null,
+        'array' => null,
+        'integer' => null,
+    ];
+
+    public static function init(): void
+    {
+        IPDO::$login = 'root';
+        IPDO::$pass  = '';
+        IPDO::$name  = 'noks_local'; // имя базы
+
+        self::$components['json']    = new JSON;
+        self::$components['integer'] = new Integer;
+        self::$components['array']   = new Array_;
+    }
+}
