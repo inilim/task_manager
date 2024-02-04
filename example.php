9ec86466cf573c56a6d87a1a790befca3c97c643
Vova iz Tambova <voller.ruden@yandex.ru>
2024-02-04 18:32:07 +0300

    add test.php

--- example.php
+++ example.php
@@ -10,27 +10,7 @@ Env::init();
 // var_dump(function_exists('integer'));
 // тут будет точка входа
 
-$task  = IPDO::exec('SELECT * FROM test LIMIT 1', 1);
-
-if ($task)
-  var_dump($task);
-else
-  echo 'Нет заданий';
-
-// echo 'Cоздание таблицы <br>';
-
-// IPDO::exec('CREATE TABLE `test` (
-//   id INT(11) NOT NULL AUTO_INCREMENT,
-//   name VARCHAR(255) NOT NULL,
-//   PRIMARY KEY(id)
-// )');
-
-// $sql = "SHOW TABLES LIKE 'test'";
-// var_dump(IPDO::exec($sql));
-
-// echo 'Удаление таблицы test <br>';
-
-// IPDO::exec('DROP TABLE `test`');
+$task  = IPDO::exec('SELECT * FROM tasks LIMIT 1', 1);
 
 // $main = new Main($task);
 
