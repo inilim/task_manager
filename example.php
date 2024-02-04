6cf7892b1eaa70b37e54c5e9e10234f3f56b3100
Vova iz Tambova <voller.ruden@yandex.ru>
2024-02-04 16:45:31 +0300

    check table exist

--- example.php
+++ example.php
@@ -8,9 +8,44 @@ use Inilim\TaskManager\Main;
 Env::init();
 
 // var_dump(function_exists('integer'));
-// exit();
 // тут будет точка входа
 
-$task  = IPDO::exec('SELECT * FROM tasks LIMIT 1', 1);
+$task  = IPDO::exec('SELECT * FROM test LIMIT 1', 1);
 
-$main = new Main($task);
+if ($task)
+  var_dump($task);
+else
+  echo 'Нет заданий';
+
+// echo 'Cоздание таблицы <br>';
+
+// IPDO::exec('CREATE TABLE `test` (
+//   id INT(11) NOT NULL AUTO_INCREMENT,
+//   name VARCHAR(255) NOT NULL,
+//   PRIMARY KEY(id)
+// )');
+
+// $sql = "SHOW TABLES LIKE 'test'";
+// var_dump(IPDO::exec($sql));
+
+// echo 'Удаление таблицы test <br>';
+
+// IPDO::exec('DROP TABLE `test`');
+
+// $main = new Main($task);
+
+use Carbon\Carbon;
+
+// $date = Carbon::now()->DateTimeZone('Europe/Moscow');
+
+// echo $date->DateTimeZone();            // fr_FR
+// echo date('m-l H:i:s', time() + 10800). '<br>';
+// echo date('m-d-Y H:i:s', strtotime("+ 3 Hour")). '<br>';
+// echo "\n";
+// printf("Now: %s", Carbon::now());
+// phpinfo();
+// echo $_SERVER['HTTP_USER_AGENT'];
+
+// $mysql->query('INSERT INTO `tasks` (`id`, `created_at`, `started_at`, `class`, `method`, `completed_at`, `params`) VALUES (NULL, '2024-02-04 09:06:26', NULL, 'Inilim\\TaskManager\\IPDO\'', 'checkStatusWork', NULL, NULL);
+
+exit();
--- src/Main.php
+++ src/Main.php
@@ -10,28 +10,28 @@ class Main
     array $task
   ) {
     $this->task = $task;
-    echo ('Проверка задания <br>');
-    echo ('Дата создания задания: ');
-    echo ($this->task['created_at']);
-    echo ('<br>');
-    echo ('Проверка контроллера: ');
+    echo 'Проверка задания <br>';
+    echo 'Дата создания задания: ';
+    echo $this->task['created_at'];
+    echo '<br>';
+    echo 'Проверка контроллера: ';
     $this->checkClass();
-    echo ('Проверка метода: ');
+    echo 'Проверка метода: ';
     $this->checkMethod();
   }
 
   public function checkClass()
   {
     if (class_exists($this->task['class'])) {
-      echo ('класс существует <br>');
-    } else echo ('ОШИБКА: класс не найден  <br>');
+      echo 'класс существует <br>';
+    } else echo 'ОШИБКА: класс не найден  <br>';
   }
 
   public function checkMethod()
   {
     if (method_exists($this->task['class'], $this->task['method'])) {
-      echo ('метод существует <br>');
-    } else echo ('ОШИБКА: метод не найден  <br>');
+      echo 'метод существует <br>';
+    } else echo 'ОШИБКА: метод не найден  <br>';
   }
 
   public function start()
