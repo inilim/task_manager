41248808bc63fa0681f0d641d58329e18dfac274
Dmitry <40324321+inilim@users.noreply.github.com>
2024-03-15 17:29:38 +0300

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,13 +1,13 @@
 CREATE TABLE `tasks` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
   `manager_id` varchar(36) DEFAULT NULL,
   `started_at` timestamp NULL DEFAULT NULL,
   `complited_at` timestamp NULL DEFAULT NULL,
   `class` varchar(255) NOT NULL,
-  `method` varchar(255) NOT NULL,
+  `method` varchar(255)  NOT NULL,
   `created_at` timestamp NOT NULL,
-  `repeat_after` int DEFAULT NULL,
-  `counter` int NOT NULL DEFAULT '0',
+  `repeat_after` int unsigned DEFAULT NULL,
+  `counter` int unsigned NOT NULL DEFAULT '0',
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `params` text,
   PRIMARY KEY (`id`)
