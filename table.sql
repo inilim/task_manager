825ce0359ba32fc37856abbdf0367500e9040ab9
Dmitry <inipel@ya.ru>
2024-10-18 17:08:34 +0300

    Update table.sql

--- table.sql
+++ table.sql
@@ -6,7 +6,7 @@ CREATE TABLE `tasks` (
   `complited_at` timestamp NULL DEFAULT NULL,
   `class` varchar(255) NOT NULL,
   `method` varchar(255)  NOT NULL,
-  `created_at` timestamp NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `repeat_after` int unsigned DEFAULT NULL COMMENT 'seconds',
   `counter` int unsigned NOT NULL DEFAULT '0',
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
